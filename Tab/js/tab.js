145569896b630eda64a8604c2c742c05ed2b42d9
Olivia jingyao <oliviachen797@gmail.com>
2022-04-02 18:12:49 +1100

    Tab1.1 only editTab need to do

--- Tab/js/tab.js
+++ Tab/js/tab.js
@@ -4,19 +4,31 @@
              //获取元素 get element 
              that = this;
              this.main = document.querySelector(id);
-             this.lis = this.main.querySelectorAll('li');
-             this.sections = this.main.querySelectorAll('section');
+             this.add = this.main.querySelector('.tabadd');
+             //获取li的父元素
+             this.ul = this.main.querySelector('.firstnav ul:first-child');
+             //获取section的父元素
+             this.fasection = this.main.querySelector('.tabscon');
              this.init();
          }
          //初始化操作 让相关的元素绑定事件 initialize 
      init() {
-             // 每个li有单独的索引号   Every li has 'index'
-             //给每一个li添加绑定事件  Every li add 'click'
-             for (var i = 0; i < this.lis.length; i++) {
+             this.updateNode()
+                 //'+'只有一个不需要循环 所以加在外边就可以了
+             this.add.onclick = this.addTab;
+             for (var i = 0; i < this.lis.length; i++) { // 每个li有单独的索引号   Every li has 'index'   //给每一个li添加绑定事件  Every li add 'click'
                  this.lis[i].index = [i];
                  this.lis[i].onclick = this.toggleTab; //不需要 toggleTab（），因为是要先click才调用，加上（）就直接调用了
+                 this.remove[i].onclick = this.removeTab; //当前的remove没有索引号，父亲li有
 
              }
+         }
+         //获取所有等价数量的li和section 还有删除按钮 动态添加元素，需要重新获取对应的元素
+     updateNode() {
+             this.lis = this.main.querySelectorAll('li');
+             this.sections = this.main.querySelectorAll('section');
+             this.remove = this.main.querySelectorAll('.fas');
+
          }
          // 切换功能 toggle 
 
@@ -30,13 +42,35 @@
      clearClass() {
              for (var i = 0; i < this.lis.length; i++) {
                  this.lis[i].className = '';
+                 this.sections[i].className = ''
              }
          }
          //---------------------留下点击的操作 清除其他样式   End---------------------
          //添加功能 add 
-     addTab() {}
+     addTab() {
+             that.clearClass();
+             var random = Math.random();
+             //1.创建li元素和section元素
+             var li = '<li class="liactive"><span>New Test</span><i class="fas fa-trash-alt"></i>';
+             var section = '<section class="conactive">Test' + random + ' </section>';
+             //2.把这两个元素追加到对应的父元素中
+             that.ul.insertAdjacentHTML('afterbegin', li);
+             that.fasection.insertAdjacentHTML('afterbegin', section);
+             that.init();
+         }
          //删除功能 remove 
-     removeTab() {}
+     removeTab(e) {
+             //阻止冒泡行为 防止触发li的的切换点击事件
+             e.stopPropagation();
+             var index = this.parentNode.index;
+             console.log(index);
+             //根据索引号删除对应的li和section
+             that.lis[index].remove();
+             that.sections[index].remove();
+             that.init();
+             index--;
+             that.lis[index] && that.lis[index].click();
+         }
          //修改功能 edit 
      editTab() {}
  }
